33488ba8ce4444ac7d5375d05deeac9cd96f2238
Samneng <samneng@marinax.co.jp>
2019-05-07 15:30:51 +0700

    add file

---
+++ asset/js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $('#gas-form').validate({
+        rules: {
+            fieldName: {
+                required: true
+            },
+            fieldEmail: {
+                required: true,
+                email: true
+            },
+            fieldSub: {
+                required: true
+            },
+            fieldMessage: {
+                required: true
+            }
+        },
+        submitHandler: function(form){
+            form.submit();
+        }
+    });
+    $('#btn-send').on('click', function(e){
+        e.preventDefault();
+        const url = 'https://script.google.com/macros/s/AKfycbwglT8GxpXDIq1zi9CrQwfZgBitEpWr5qGCmFNE-mmr4Z-01Lo/exec';
+        let getName = $('#name').val(),
+            getEmail = $('#email').val(),
+            getSub = $('#subject').val(),
+            getMsg = $('#message').val();
+        const postRequest = {
+            name: getName,
+            email: getEmail,
+            subject: getSub,
+            message: getMsg
+        };
+        console.log(postRequest);
+        if(url) {
+            $.post(url, JSON.stringify(postRequest)).then(res => {
+                $('#form-msg').text(res.msg);
+            });
+        }
+    });
+});
+
+// function sendEmail(e) {
+//     e.preventDefault();
+//     const url = "";
+//     const postRequest = {
+//         name: e.target['fieldName'].value,
+//         email: e.target['fieldEmail'].value,
+//         subject: e.target['fieldSub'].value,
+//         message: e.target['fieldMessage'].value
+//     };
+//     console.log(postRequest);
+//     if(url) {
+//         $.post(url, JSON.stringify(postRequest)).then(res => {
+//             e.target.reset();
+//             alert(res.msg);
+//         });
+//     }
+// }
